47d75ab421b1001e6f1d9b01bc89293f71e42729
15530191420 <1214431810@qq.com>
2020-10-24 09:35:55 +0800

    第一次上传

---
+++ src/main/java/com/megagao/production/ssm/service/BranchService.java
@@ -0,0 +1,18 @@
+package com.megagao.production.ssm.service;
+
+import java.util.List;
+
+import com.megagao.production.ssm.domain.Branch;
+import com.megagao.production.ssm.domain.customize.EUDataGridResult;
+
+public interface BranchService {
+	public List<Branch> find();
+	/**
+	 * 分页查询插件
+	 * @param page
+	 * @param rows
+	 * @return
+	 * @throws Exception
+	 */
+	EUDataGridResult getList(int page,int rows) throws Exception;
+}
